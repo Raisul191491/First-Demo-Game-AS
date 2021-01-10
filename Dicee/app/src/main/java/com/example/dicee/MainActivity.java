c64317d7f7e6235d9ab17df318f848e4cd67f5b2
Md. Raisul Islam <52915635+Raisul191491@users.noreply.github.com>
2021-01-10 13:15:06 +0600

    Face complete

--- Dicee/app/src/main/java/com/example/dicee/MainActivity.java
+++ Dicee/app/src/main/java/com/example/dicee/MainActivity.java
@@ -3,6 +3,12 @@ package com.example.dicee;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int[] diceArray = {
+                R.drawable.dice1,
+                R.drawable.dice2,
+                R.drawable.dice3,
+                R.drawable.dice4,
+                R.drawable.dice5,
+                R.drawable.dice6,
+        };
+
+        Button rollButton;
+        rollButton =  findViewById(R.id.roll_button);
+
+        ImageView leftDice = findViewById(R.id.image_Leftdice);
+        ImageView rightDice = findViewById(R.id.image_Rightdice);
+
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Dicee","This is gay" );
+
+                Random randgen = new Random();
+
+                int number = randgen.nextInt(6);
+                Log.d("Dicee","The random number is "+ number);
+            }
+        });
     }
 }
